5c7fe568e4ea1b3d13ddff17d0edd07c3e3c790b
LeoCavan <157677656+LeoCavan@users.noreply.github.com>
2024-06-08 02:28:47 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// var carousel = document.getElementById('projectCarousel');
+//     var carouselItems = carousel.getElementsByClassName('carousel-item');
+
+//     // Add active class to the first item
+//     carouselItems
+
+//     const carouselItems = document.querySelectorAll('.carousel-item');
+
